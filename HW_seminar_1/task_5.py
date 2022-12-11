510a341e344e9584ceacb9870a41ff495ce06dbe
Royal00Blood <nazgoool1@gmail.com>
2022-12-11 15:26:37 +0400

    complit HW

--- HW_seminar_1/task_5.py
+++ HW_seminar_1/task_5.py
@@ -1,7 +1,16 @@
 # Напишите программу, которая принимает на вход координаты двух точек и
 # находит расстояние между ними в 2D пространстве.
+print("Point number 1:")
+x_1 = int(input("Enter x-coordinate --> "))
+y_1 = int(input("Enter y-coordinate --> "))
+print("Point number 2")
+x_2 = int(input("Enter x-coordinate --> "))
+y_2 = int(input("Enter y-coordinate --> "))
 
-x = int(input("Enter x-coordinate --> "))
-y = int(input("Enter y-coordinate --> "))
-x = int(input("Enter x-coordinate --> "))
-y = int(input("Enter y-coordinate --> "))
+
+# DBP - distance_between_points
+def DBP(x1, y1, x2, y2):
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** (1 / 2)
+
+
+print("distance_between_points = ","%.2f" % (DBP(x_1, y_1, x_2, y_2)))
