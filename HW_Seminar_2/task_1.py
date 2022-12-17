96805970d9657917631f64dec8fb909550f98d90
Royal00Blood <nazgoool1@gmail.com>
2022-12-17 16:28:08 +0400

    HW task 1 complit

--- HW_Seminar_2/task_1.py
+++ HW_Seminar_2/task_1.py
@@ -5,3 +5,13 @@
 #
 # - 6782 -> 23
 # - 0,56 -> 11
+N = float(input("Ввведите число: "))
+def Sum_in_numbers(num, sum=0):
+    while num % 1 > 0:
+        num *= 10
+    num_1 = int(num)
+    while (num_1 / 10 > 0 and num_1 > 0):
+        sum += int(num_1 % 10)
+        num_1 = num_1 / 10
+    return sum
+print(f'Сумма чисел числа = {Sum_in_numbers(N)}')
