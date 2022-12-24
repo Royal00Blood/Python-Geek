285622fff6da35893590225beea5de5b7af9eb8e
Royal00Blood <nazgoool1@gmail.com>
2022-12-24 11:27:06 +0400

    seminar 3 HW solved 4 tasks

--- HW_Seminar_3/task_5.py
+++ HW_Seminar_3/task_5.py
@@ -1,21 +1,26 @@
-count = int(input('введите номер элемента фибоначи : '))
-def fibonachi_solve(n):
-    fib1 = 1
-    fib2 = 1
-    if n>2:
-        i = 0
-        while i < n - 2:
-            fib_sum = fib1 + fib2
-            fib1 = fib2
-            fib2 = fib_sum
-            i = i + 1
-    return fib2
-def complit_list_fib(n):
-    list_fib = []
-    for i in range(n):
-        list_fib.append(fibonachi_solve(n-i)*(-1)**(i+1))
-    list_fib.append(0)
-    for i in range(1,n+1):
-        list_fib.append(fibonachi_solve(i))
-    return list_fib
-print(complit_list_fib(count))
+# Задайте число. Составьте список чисел Фибоначчи,
+# в том числе для отрицательных индексов.
+#
+# Пример:
+#&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+# - для k = 8 список будет выглядеть так:
+# [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+def fibonacci(n):
+    fib1 = fib2 = 1
+    n = n - 2
+    while n > 0:
+        fib1, fib2 = fib2, fib1 + fib2
+    n -= 1
+    return n
+def fibonacci_complit_list(N):
+    new_list = []
+    for i in range(2*N+1):
+        if i <= N:
+            new_list.append(fibonacci(N-i)*(-1)**(N-i))
+        elif i == N+1:
+            new_list.append(0)
+        else:
+            new_list.append(fibonacci(i))
+    return new_list
+number = int(input('«Номер элемента ряда Фибоначчи:'))
+print(fibonacci_complit_list(number))
