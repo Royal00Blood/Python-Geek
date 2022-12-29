4fef7c983094cd90d1c31471d869e105ea15ade9
Ivan <nazgoool1@gmail.com>
2022-12-29 11:58:39 +0400

    add chenge

--- HW_Seminar_4/task_3.py
+++ HW_Seminar_4/task_3.py
@@ -1,2 +1,13 @@
 # Задайте последовательность чисел.
-# Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+# Напишите программу, которая
+# выведет список неповторяющихся элементов исходной последовательности.
+numbers_list = [int(el) for el in input('Enter numbers: ').split()]
+elements = []
+def Print_universal_element(nums):
+    elements = []
+    for i in range(len(nums)):
+        if not(nums[i] in elements):
+            elements.append(nums[i])
+    print(elements)
+
+Print_universal_element(numbers_list)
